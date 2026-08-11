72f909428fc34f0729f9e94864d3e00b2613adcb
João Mendes <joaoluiizmendes@gmail.com>
2026-08-11 14:00:19 -0300

    add manualChunks in Vite for vendor bundle splitting (react, motion, icons, i18n, ogl, spline)

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react':  ['react', 'react-dom'],
+          'vendor-motion': ['framer-motion'],
+          'vendor-icons':  ['react-icons'],
+          'vendor-i18n':   ['i18next', 'react-i18next'],
+          'vendor-ogl':    ['ogl'],
+          'vendor-spline': ['@splinetool/react-spline', '@splinetool/runtime'],
+        },
+      },
+    },
+  },
 })
